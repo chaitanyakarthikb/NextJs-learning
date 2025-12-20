cac8550bbada95032b587490b5de5c97582a47e8
Chaitanya Karthik <chaitanyakarthikbasva@gmail.com>
2025-12-20 14:00:37 +0530

    MONGODB SETUP, CONNECTIONS , AND FETCHING FROM DB

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,18 +1,19 @@
-'use client'
-import Link from 'next/link'
 import React from 'react'
-import Properties from '../../properties.json';
 import PropertyCard from '@/components/PropertyCard';
+import connectToDB from '@/config/database';
+import Properties from '@/models/Property';
 
 
-const page = () => {
+const page = async() => {
+  await connectToDB();
+  const properties = await Properties.find({}).lean();
   return (
     <section className=' px-4 py-6'>
       <div className='lg:container max-w-12xl  px-4 py-6 grid grid-cols-1 md:grid-cols-3 gap-12 mx-auto'>
-       {Properties.length === 0 ? (
-        <h1>No Properties FOund...</h1>
+       {properties.length === 0 ? (
+        <h1>No Properties Found...</h1>
        ) : (
-        Properties.map((property)=>{
+        properties.map((property)=>{
           return(
             <PropertyCard key={property._id} property={property}/>
           )
